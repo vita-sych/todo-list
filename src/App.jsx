5ada47efdb90becf82468401de255722fd23dfd2
VitaSych <vita.sych.vs@gmail.com>
2025-07-28 14:43:28 -0700

    add TodoListItem component and state for new Todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css';
 import TodoList from './TodoList.jsx';
 import TodoForm from './TodoForm.jsx';
+import { useState } from 'react';
 
 function App() {
+  const initialValue = 'Example Text';
+  const [newTodo, setNewTodo] = useState(initialValue);
+
   return (
     <div>
       <h1>To do list</h1>
       <TodoForm></TodoForm>
+      <p>{newTodo}</p>
       <TodoList></TodoList>
     </div>
   );
